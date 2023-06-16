f9eeac61a409116e66fb37ad1cc3f4fcb7c9b709
brianMunyao <brianmunyao6@gmail.com>
2023-06-16 16:28:57 +0300

    use dynamic hostname

--- frontend/src/app/components/aboutus/aboutus.component.ts
+++ frontend/src/app/components/aboutus/aboutus.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
-import { JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { JsonPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-aboutus',
   standalone: true,
   templateUrl: './aboutus.component.html',
   styleUrls: ['./aboutus.component.css'],
-  imports: [JsonPipe]
+  imports: [CommonModule, JsonPipe],
 })
 export class AboutusComponent implements OnInit {
-  info!:{name:string,age:number}
+  info!: { name: string; age: number };
 
-  constructor(private client:HttpClient){
-
-  }
+  constructor(private client: HttpClient) {}
 
   ngOnInit(): void {
-    this.client.get<{name:string,age:number}>('http://localhost:3000').subscribe(info=> this.info=info)
+    this.client
+      .get<{ name: string; age: number }>(
+        `http://${window.location.hostname}:3000`
+      )
+      .subscribe((info) => (this.info = info));
   }
 }
